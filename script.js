edee8565fff4c6bd59c16ac6e4d0391109e2116a
JD <janelle.m.deane@gmail.com>
2020-09-22 09:10:17 -0700

    completed 5 day forecast

--- script.js
+++ script.js
@@ -42,17 +42,65 @@ submitBtn.on("click", function(event){
       var wind= $("#wind");
       wind.text("Wind Speed: "+ response.list[0].wind.speed + "MPH");
   
-    for (var i = 8; i < 41; i+8) {
-      var icon=response.list[i].weather[0].icon
+    // 5 Day Forecast puils request and adds it to the page
+      var icon=response.list[8].weather[0].icon
       console.log(icon);
       var iconUrl ="http://openweathermap.org/img/w/" + icon + ".png";
-      weatherIcon=$(`.weatherPic-${i}`).attr("src", iconUrl);
-      // var date5=$("date5").text(moment().add(4, 'day').format('L'))
-      // card.append(weatherIcon)
-    }
-  });
+      weatherIcon=$(`.weatherPic-8`).attr("src", iconUrl);
+      var date8=$(".date8");
+      date8.text(moment().add(1, 'day').format('L'))
+      var temp8= $(".temp8");
+      temp8.text("Temperature: "+ response.list[8].main.temp + "F")
+      var humidity8= $(".humidity8");
+      humidity8.text("Humidity: "+ response.list[8].main.humidity+"%");
+  
+      var icon=response.list[16].weather[0].icon
+      console.log(icon);
+      var iconUrl ="http://openweathermap.org/img/w/" + icon + ".png";
+      weatherIcon=$(`.weatherPic-16`).attr("src", iconUrl);
+      var date16=$(".date16");
+      date16.text(moment().add(2, 'day').format('L'));
+      var temp16= $(".temp16");
+      temp16.text("Temperature: "+ response.list[16].main.temp + "F")
+      var humidity16= $(".humidity16");
+      humidity16.text("Humidity: "+ response.list[16].main.humidity+"%");
+  
+      var icon=response.list[24].weather[0].icon
+      console.log(icon);
+      var iconUrl ="http://openweathermap.org/img/w/" + icon + ".png";
+      weatherIcon=$(`.weatherPic-24`).attr("src", iconUrl);
+      var date24=$(".date24");
+      date24.text(moment().add(3, 'day').format('L'));
+      var temp24= $(".temp24");
+      temp24.text("Temperature: "+ response.list[24].main.temp + "F")
+      var humidity24= $(".humidity24");
+      humidity24.text("Humidity: "+ response.list[24].main.humidity+"%");
+  
+      var icon=response.list[32].weather[0].icon
+      console.log(icon);
+      var iconUrl ="http://openweathermap.org/img/w/" + icon + ".png";
+      weatherIcon=$(`.weatherPic-32`).attr("src", iconUrl);
+      var date32=$(".date32");
+      date32.text(moment().add(4, 'day').format('L'));
+      var temp32= $(".temp32");
+      temp32.text("Temperature: "+ response.list[32].main.temp + "F")
+      var humidity32= $(".humidity32");
+      humidity32.text("Humidity: "+ response.list[32].main.humidity+"%");
+  
+      var icon=response.list[39].weather[0].icon
+      console.log(icon);
+      var iconUrl ="http://openweathermap.org/img/w/" + icon + ".png";
+      weatherIcon=$(`.weatherPic-40`).attr("src", iconUrl);
+      var date40=$(".date40");
+      date40.text(moment().add(5, 'day').format('L'));
+      var temp40= $(".temp40");
+      temp40.text("Temperature: "+ response.list[39].main.temp + "F")
+      var humidity40= $(".humidity40");
+      humidity40.text("Humidity: "+ response.list[39].main.humidity+"%");
+  
+    });
 })
 //take input from search input
 //save city search to local storage and append page
 
-//take 5 day data and put images on page
+
